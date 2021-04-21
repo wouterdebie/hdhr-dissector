8a0458753d15ee456293737dc79141820af5613c
Wouter de Bie <wouter.de.bie@datadoghq.com>
2021-04-20 19:18:54 -0500

    Make it compile for Wireshark 3

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 hdhr-dissector
 ==============
 
-Wireshark dissector plugin for the HDHomeRun protocol
+Wireshark/tshark Plugin in C for [HDHomerun Packets](https://github.com/Silicondust/libhdhomerun/blob/master/hdhomerun_pkt.h).
+
+NOTE: This for makes the dissector work for wireshark 3.
+# Build
+
+- Download Wireshark source-code.
+- Create __hdhr__ directory inside __wireshark/plugins/epan__ folder.
+- Download/Clone source code from this repo into the __hdhr__ folder.
+- Inside __wireshark__ folder, create __CMakeListsCustom.txt__ and add the line.
+```
+set(CUSTOM_PLUGIN_SRC_DIR plugins/epan/hdhr)
+```
+- Follow the build instructions of Wireshark for your OS setup
+
+# Notes
+
+- This code has been tested with latest stable release of Wireshark (3.2.0) on MacOS
+
--- packet-hdhr.c
+++ packet-hdhr.c
@@ -28,71 +28,76 @@
 #include <epan/packet.h>
 #include <epan/ipproto.h>
 #include <epan/prefs.h>
+#include <epan/tvbuff.h>
 
-#define HDHR_PORT       65001
+#define HDHR_PORT 65001
 #define HDHR_MIN_LENGTH 8
 
 /* Initialize the protocol and registered fields */
-static int proto_hdhr          = -1;
-static int hf_hdhr_type        = -1;
-static int hf_hdhr_len         = -1;
-static int hf_hdhr_chk         = -1;
-static int hf_payload          = -1;
-static int hf_pay_tag          = -1;
-static int hf_pay_len          = -1;
-static int hf_pay_disc_type    = -1;
-static int hf_pay_disc_id      = -1;
-static int hf_pay_getset_name  = -1;
+static int proto_hdhr = -1;
+static int hf_hdhr_type = -1;
+static int hf_hdhr_len = -1;
+static int hf_hdhr_chk = -1;
+static int hf_payload = -1;
+static int hf_pay_tag = -1;
+static int hf_pay_len = -1;
+static int hf_pay_disc_type = -1;
+static int hf_pay_disc_id = -1;
+static int hf_pay_getset_name = -1;
 static int hf_pay_getset_value = -1;
 static int hf_pay_getset_error = -1;
-static int hf_upgrade_offset   = -1;
-static int hf_upgrade_data     = -1;
+static int hf_pay_base_url = -1;
+static int hf_pay_lineup_url = -1;
+static int hf_pay_tuner_count = -1;
+static int hf_upgrade_offset = -1;
+static int hf_upgrade_data = -1;
 
 /* Initialize the subtree pointers */
-static gint ett_hdhr    = -1;
+static gint ett_hdhr = -1;
 static gint ett_payload = -1;
 
 #define DISCOVER_REQ 0x0002
 #define DISCOVER_RPY 0x0003
-#define GETSET_REQ   0x0004
-#define GETSET_RPY   0x0005
-#define UPGRADE_REQ  0x0006
-#define UPGRADE_RPY  0x0007
+#define GETSET_REQ 0x0004
+#define GETSET_RPY 0x0005
+#define UPGRADE_REQ 0x0006
+#define UPGRADE_RPY 0x0007
 static const value_string packet_type_names[] = {
-    { DISCOVER_REQ, "Discover Request" },
-    { DISCOVER_RPY, "Discover Reply"   },
-    { GETSET_REQ,   "Get/Set Request"  },
-    { GETSET_RPY,   "Get/Set Reply"    },
-    { UPGRADE_REQ,  "Upgrade Request"  },
-    { UPGRADE_RPY,  "Upgrade Reply"    },
-    { 0,            NULL               }
-};
+    {DISCOVER_REQ, "Discover Request"},
+    {DISCOVER_RPY, "Discover Reply"},
+    {GETSET_REQ, "Get/Set Request"},
+    {GETSET_RPY, "Get/Set Reply"},
+    {UPGRADE_REQ, "Upgrade Request"},
+    {UPGRADE_RPY, "Upgrade Reply"},
+    {0, NULL}};
 
-#define DEVICE_TYPE    0x01
-#define DEVICE_ID      0x02
-#define GETSET_NAME    0x03
-#define GETSET_VALUE   0x04
+#define DEVICE_TYPE 0x01
+#define DEVICE_ID 0x02
+#define GETSET_NAME 0x03
+#define GETSET_VALUE 0x04
 #define GETSET_LOCKKEY 0x15
-#define ERROR_MSG      0x05
-#define TUNER_COUNT    0x10
+#define ERROR_MSG 0x05
+#define TUNER_COUNT 0x10
+#define BASE_URL 0x2A
+#define LINEUP_URL 0x27
 static const value_string payload_tag_names[] = {
-    { DEVICE_TYPE,    "Device Type"      },
-    { DEVICE_ID,      "Device Id"        },
-    { GETSET_NAME,    "Get/Set Name"     },
-    { GETSET_VALUE,   "Get/Set Value"    },
-    { GETSET_LOCKKEY, "Get/Set Lock Key" },
-    { ERROR_MSG,      "Error Message"    },
-    { TUNER_COUNT,    "Tuner Count"      },
-    { 0,              NULL               }
-};
+    {DEVICE_TYPE, "Device Type"},
+    {DEVICE_ID, "Device Id"},
+    {GETSET_NAME, "Get/Set Name"},
+    {GETSET_VALUE, "Get/Set Value"},
+    {GETSET_LOCKKEY, "Get/Set Lock Key"},
+    {ERROR_MSG, "Error Message"},
+    {TUNER_COUNT, "Tuner Count"},
+    {BASE_URL, "Base URL"},
+    {LINEUP_URL, "Lineup URL"},
+    {0, NULL}};
 
 #define DEVICE_TYPE_TUNER 0x00000001
-#define DEVICE_TYPE_WILD  0xFFFFFFFF
+#define DEVICE_TYPE_WILD 0xFFFFFFFF
 static const value_string discover_type_names[] = {
-    { DEVICE_TYPE_TUNER, "Tuner" },
-    { DEVICE_TYPE_WILD,  "Any"   },
-    { 0,                 NULL    }
-};
+    {DEVICE_TYPE_TUNER, "Tuner"},
+    {DEVICE_TYPE_WILD, "Any"},
+    {0, NULL}};
 
 static int
 dissect_hdhr(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
@@ -102,29 +107,29 @@ dissect_hdhr(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_
     proto_tree *pay_tree;
 
     guint offset = 0;
-    guint length = tvb_length(tvb);
+    guint length = tvb_captured_length(tvb);
     guint checksum_off = length - 4;
 
     guint16 packet_type = tvb_get_ntohs(tvb, 0);
     const gchar *packet_type_str;
 
-    guint8  pay_tag;
+    guint8 pay_tag;
     guint16 pay_len;
     guint32 upgrade_offset;
 
     /*** HEURISTICS ***/
-    if (tvb_length(tvb) < HDHR_MIN_LENGTH)
+    if (tvb_captured_length(tvb) < HDHR_MIN_LENGTH)
         return 0;
-    if (pinfo->ipproto == IP_PROTO_UDP &&
-        !(packet_type == DISCOVER_REQ ||
-          packet_type == DISCOVER_RPY))
-        return 0;
-    if (pinfo->ipproto == IP_PROTO_TCP &&
-        !(packet_type == GETSET_REQ  ||
-          packet_type == GETSET_RPY  ||
-          packet_type == UPGRADE_REQ ||
-          packet_type == UPGRADE_RPY))
-        return 0;
+    // if (pinfo->ipproto == IP_PROTO_UDP &&
+    //     !(packet_type == DISCOVER_REQ ||
+    //       packet_type == DISCOVER_RPY))
+    //     return 0;
+    // if (pinfo->ipproto == IP_PROTO_TCP &&
+    //     !(packet_type == GETSET_REQ ||
+    //       packet_type == GETSET_RPY ||
+    //       packet_type == UPGRADE_REQ ||
+    //       packet_type == UPGRADE_RPY))
+    //     return 0;
 
     col_set_str(pinfo->cinfo, COL_PROTOCOL, "HDHR");
 
@@ -145,72 +150,86 @@ dissect_hdhr(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_
 
     switch (packet_type)
     {
-        case DISCOVER_REQ:
-        case DISCOVER_RPY:
-        case GETSET_REQ:
-        case GETSET_RPY:
-            while (offset < checksum_off)
+    case DISCOVER_REQ:
+    case DISCOVER_RPY:
+    case GETSET_REQ:
+    case GETSET_RPY:
+        while (offset < checksum_off)
+        {
+            ti = proto_tree_add_item(hdhr_tree, hf_payload, tvb, offset, -1, ENC_NA);
+            pay_tree = proto_item_add_subtree(ti, ett_payload);
+            pay_tag = tvb_get_guint8(tvb, offset);
+            proto_tree_add_uint(pay_tree, hf_pay_tag, tvb, offset, 1, pay_tag);
+            offset++;
+
+            pay_len = (guint16)tvb_get_guint8(tvb, offset);
+            if ((pay_len & 0x80) == 0)
             {
-                ti = proto_tree_add_item(hdhr_tree, hf_payload, tvb, offset, -1, ENC_NA);
-                pay_tree = proto_item_add_subtree(ti, ett_payload);
-                pay_tag = tvb_get_guint8(tvb, offset);
-                proto_tree_add_uint(pay_tree, hf_pay_tag, tvb, offset, 1, pay_tag);
+                proto_tree_add_uint(pay_tree, hf_pay_len, tvb, offset, 1, pay_len);
+                proto_item_set_len(ti, 2 + pay_len);
                 offset++;
-
-                pay_len = (guint16)tvb_get_guint8(tvb, offset);
-                if ((pay_len & 0x80) == 0)
-                {
-                    proto_tree_add_uint(pay_tree, hf_pay_len, tvb, offset, 1, pay_len);
-                    proto_item_set_len(ti, 2 + pay_len);
-                    offset++;
-                } else {
-                    pay_len = (pay_len & 0x7F) + ((guint16)tvb_get_guint8(tvb, offset + 1) << 7);
-                    proto_tree_add_uint(pay_tree, hf_pay_len, tvb, offset, 2, pay_len);
-                    proto_item_set_len(ti, 3 + pay_len);
-                    offset += 2;
-                }
-
-                if (offset + pay_len <= checksum_off)
-                {
-                    switch (pay_tag)
-                    {
-                        case DEVICE_TYPE:
-                            proto_tree_add_item(pay_tree, hf_pay_disc_type, tvb, offset, pay_len, ENC_BIG_ENDIAN);
-                            break;
-                        case DEVICE_ID:
-                            proto_tree_add_item(pay_tree, hf_pay_disc_id, tvb, offset, pay_len, ENC_BIG_ENDIAN);
-                            break;
-                        case GETSET_NAME:
-                            proto_tree_add_item(pay_tree, hf_pay_getset_name, tvb, offset, pay_len, ENC_NA);
-                            break;
-                        case GETSET_VALUE:
-                            proto_tree_add_item(pay_tree, hf_pay_getset_value, tvb, offset, pay_len, ENC_NA);
-                            break;
-                        case ERROR_MSG:
-                            proto_tree_add_item(pay_tree, hf_pay_getset_error, tvb, offset, pay_len, ENC_NA);
-                            break;
-                    } // end switch (pay_tag)
-                    offset += pay_len;
-                } else {
-                    // todo: malformed packet
-                    offset = checksum_off;
-                }
-            } // end while (offset < checksum_off)
-            break;
-
-        case UPGRADE_REQ:
-            upgrade_offset = tvb_get_ntohl(tvb, offset);
-            proto_tree_add_uint(hdhr_tree, hf_upgrade_offset, tvb, offset, 4, upgrade_offset);
-            offset += 4;
-            if (upgrade_offset != 0xFFFFFFFF) {
-                proto_tree_add_item(hdhr_tree, hf_upgrade_data, tvb, offset, 256, ENC_BIG_ENDIAN);
-                offset += 256;
             }
-            break;
+            else
+            {
+                pay_len = (pay_len & 0x7F) + ((guint16)tvb_get_guint8(tvb, offset + 1) << 7);
+                proto_tree_add_uint(pay_tree, hf_pay_len, tvb, offset, 2, pay_len);
+                proto_item_set_len(ti, 3 + pay_len);
+                offset += 2;
+            }
 
-        case UPGRADE_RPY:
-            // todo...
-            break;
+            if (offset + pay_len <= checksum_off)
+            {
+                switch (pay_tag)
+                {
+                case DEVICE_TYPE:
+                    proto_tree_add_item(pay_tree, hf_pay_disc_type, tvb, offset, pay_len, ENC_BIG_ENDIAN);
+                    break;
+                case DEVICE_ID:
+                    proto_tree_add_item(pay_tree, hf_pay_disc_id, tvb, offset, pay_len, ENC_BIG_ENDIAN);
+                    break;
+                case GETSET_NAME:
+                    proto_tree_add_item(pay_tree, hf_pay_getset_name, tvb, offset, pay_len, ENC_NA);
+                    break;
+                case GETSET_VALUE:
+                    proto_tree_add_item(pay_tree, hf_pay_getset_value, tvb, offset, pay_len, ENC_NA);
+                    break;
+                case ERROR_MSG:
+                    proto_tree_add_item(pay_tree, hf_pay_getset_error, tvb, offset, pay_len, ENC_NA);
+                    break;
+                case BASE_URL:
+                    proto_tree_add_item(pay_tree, hf_pay_base_url, tvb, offset, pay_len, ENC_NA);
+                    break;
+                case LINEUP_URL:
+                    proto_tree_add_item(pay_tree, hf_pay_lineup_url, tvb, offset, pay_len, ENC_NA);
+                    break;
+                case TUNER_COUNT:
+                    proto_tree_add_item(pay_tree, hf_pay_tuner_count, tvb, offset, pay_len, ENC_BIG_ENDIAN);
+                    break;
+                } // end switch (pay_tag)
+                offset += pay_len;
+            }
+            else
+            {
+                // todo: malformed packet
+                offset = checksum_off;
+            }
+        } // end while (offset < checksum_off)
+        break;
+
+    case UPGRADE_REQ:
+        upgrade_offset = tvb_get_ntohl(tvb, offset);
+        proto_tree_add_uint(hdhr_tree, hf_upgrade_offset, tvb, offset, 4, upgrade_offset);
+        offset += 4;
+        if (upgrade_offset != 0xFFFFFFFF)
+        {
+            proto_tree_add_item(hdhr_tree, hf_upgrade_data, tvb, offset, 256, ENC_BIG_ENDIAN);
+            offset += 256;
+        }
+        break;
+
+    case UPGRADE_RPY:
+        // todo...
+        break;
     } // end switch (packet_type)
 
     proto_tree_add_item(hdhr_tree, hf_hdhr_chk, tvb, checksum_off, 4, ENC_LITTLE_ENDIAN);
@@ -218,81 +237,77 @@ dissect_hdhr(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_
     return offset;
 }
 
-void
-proto_register_hdhr(void)
+void proto_register_hdhr(void)
 {
     static hf_register_info hf[] = {
-        { &hf_hdhr_type,
-            { "Type", "hdhr.type",
-              FT_UINT16, BASE_DEC, VALS(packet_type_names), 0x0,
-              NULL, HFILL }
-        },
-        { &hf_hdhr_len,
-            { "Length", "hdhr.len",
-              FT_UINT16, BASE_DEC, NULL, 0x0,
-              NULL, HFILL }
-        },
-        { &hf_hdhr_chk,
-            { "Checksum", "hdhr.crc",
-              FT_UINT32, BASE_HEX, NULL, 0x0,
-              NULL, HFILL }
-        },
-        { &hf_payload,
-            { "Payload", "hdhr.pay",
-              FT_NONE, BASE_NONE, NULL, 0x0,
-              NULL, HFILL }
-        },
-        { &hf_pay_tag,
-            { "Tag", "hdhr.pay.tag",
-              FT_UINT8, BASE_DEC, VALS(payload_tag_names), 0x0,
-              NULL, HFILL }
-        },
-        { &hf_pay_len,
-            { "Length", "hdhr.pay.len",
-               FT_UINT16, BASE_DEC, NULL, 0x0,
-               NULL, HFILL }
-        },
-        { &hf_pay_disc_type,
-            { "Type", "hdhr.pay.disc.type",
-              FT_UINT32, BASE_HEX, VALS(discover_type_names), 0x0,
-              NULL, HFILL }
-        },
-        { &hf_pay_disc_id,
-            { "Type", "hdhr.pay.disc.id",
-              FT_UINT32, BASE_HEX, NULL, 0x0,
-              NULL, HFILL }
-        },
-        { &hf_pay_getset_name,
-            { "Name", "hdhr.pay.gs.name",
-              FT_STRINGZ, BASE_NONE, NULL, 0x0,
-              NULL, HFILL }
-        },
-        { &hf_pay_getset_value,
-            { "Value", "hdhr.pay.gs.value",
-              FT_STRINGZ, BASE_NONE, NULL, 0x0,
-              NULL, HFILL }
-        },
-        { &hf_pay_getset_error,
-            { "Error", "hdhr.pay.gs.err",
-              FT_STRINGZ, BASE_NONE, NULL, 0x0,
-              NULL, HFILL }
-        },
-        { &hf_upgrade_offset,
-            { "Position", "hdhr.up.off",
-              FT_UINT32, BASE_HEX, NULL, 0x0,
-              NULL, HFILL }
-        },
-        { &hf_upgrade_data,
-            { "Data", "hdhr.up.data",
-              FT_BYTES, BASE_NONE, NULL, 0x0,
-              NULL, HFILL }
-        }
-    };
+        {&hf_hdhr_type,
+         {"Type", "hdhr.type",
+          FT_UINT16, BASE_DEC, VALS(packet_type_names), 0x0,
+          NULL, HFILL}},
+        {&hf_hdhr_len,
+         {"Length", "hdhr.len",
+          FT_UINT16, BASE_DEC, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_hdhr_chk,
+         {"Checksum", "hdhr.crc",
+          FT_UINT32, BASE_HEX, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_payload,
+         {"Payload", "hdhr.pay",
+          FT_NONE, BASE_NONE, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_pay_tag,
+         {"Tag", "hdhr.pay.tag",
+          FT_UINT8, BASE_DEC, VALS(payload_tag_names), 0x0,
+          NULL, HFILL}},
+        {&hf_pay_len,
+         {"Length", "hdhr.pay.len",
+          FT_UINT16, BASE_DEC, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_pay_disc_type,
+         {"Type", "hdhr.pay.disc.type",
+          FT_UINT32, BASE_HEX, VALS(discover_type_names), 0x0,
+          NULL, HFILL}},
+        {&hf_pay_disc_id,
+         {"Type", "hdhr.pay.disc.id",
+          FT_UINT32, BASE_HEX, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_pay_getset_name,
+         {"Name", "hdhr.pay.gs.name",
+          FT_STRINGZ, BASE_NONE, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_pay_getset_value,
+         {"Value", "hdhr.pay.gs.value",
+          FT_STRINGZ, BASE_NONE, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_pay_getset_error,
+         {"Error", "hdhr.pay.gs.err",
+          FT_STRINGZ, BASE_NONE, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_pay_base_url,
+         {"Value", "hdhr.pay.gs.base_url",
+          FT_STRINGZ, BASE_NONE, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_pay_lineup_url,
+         {"Value", "hdhr.pay.gs.lineup_url",
+          FT_STRINGZ, BASE_NONE, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_upgrade_offset,
+         {"Position", "hdhr.up.off",
+          FT_UINT32, BASE_HEX, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_pay_tuner_count,
+         {"Value", "hdhr.pay.gs.tuner_count",
+          FT_UINT8, BASE_HEX, NULL, 0x0,
+          NULL, HFILL}},
+        {&hf_upgrade_data,
+         {"Data", "hdhr.up.data",
+          FT_BYTES, BASE_NONE, NULL, 0x0,
+          NULL, HFILL}}};
 
     static gint *ett[] = {
         &ett_hdhr,
-        &ett_payload
-    };
+        &ett_payload};
 
     proto_hdhr = proto_register_protocol("HDHomeRun", "HDHR", "hdhr");
 
@@ -301,10 +316,9 @@ proto_register_hdhr(void)
     prefs_register_protocol(proto_hdhr, NULL);
 }
 
-void
-proto_reg_handoff_hdhr(void)
+void proto_reg_handoff_hdhr(void)
 {
-    dissector_handle_t hdhr_handle = new_create_dissector_handle(dissect_hdhr, proto_hdhr);
+    dissector_handle_t hdhr_handle = create_dissector_handle(dissect_hdhr, proto_hdhr);
     dissector_add_uint("tcp.port", HDHR_PORT, hdhr_handle);
     dissector_add_uint("udp.port", HDHR_PORT, hdhr_handle);
 }
